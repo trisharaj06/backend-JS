8828740969ee80e6749b05267e098f28b1cc3bc2
trisharaj06 <trirox2030@gmail.com>
2024-12-30 17:15:38 +0530

    updating async handler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,15 +1,4 @@
-const asyncHandler = (fn) => async(req,res,next) =>{
-  try{
-    await fn(req,res,next)
-  } catch(error){
-    res.status(error.code || 500).json({
-      success: false,
-      message: error.message
-    })
-  }
-}
-
-const asyncHandler2 = (requestHandler) =>{
+const asyncHandler = (requestHandler) =>{
   (req,res,next)=>{
     Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err))
   }

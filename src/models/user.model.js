d05e4561e87ada6d74e9e9f23292fa64ae130e34
trisharaj06 <trirox2030@gmail.com>
2025-01-02 11:53:00 +0530

    add token logic

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, {Schema} from "mongoose";
+import { emit } from "nodemon";
 
 const userSchema = new Schema({
   userName: {
@@ -41,5 +42,43 @@ const userSchema = new Schema({
     type: String
   },
 },{timestamps:true})
+
+// avoid using arrow functions here
+
+userSchema.pre("save",async function (next) {
+  if(!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password,10)
+  next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+  return JsonWebTokenError.sign({
+    _id: this._id,
+    email: this.email,
+    userName: this.userName,
+    fullName: this.fullName
+  },
+  process.env.ACCESS_TOKEN_SECRET,
+  {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+  }
+  )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+  return JsonWebTokenError.sign({
+    _id: this._id,
+  },
+  process.env.REFRESH_TOKEN_SECRET,
+  {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+  }
+  )
+}
 
 export const User = mongoose.model("User",userSchema)

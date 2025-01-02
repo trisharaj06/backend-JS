6e41b50a84a1e0dc25964ae4bc60ea1d788769ec
trisharaj06 <trirox2030@gmail.com>
2025-01-03 00:05:04 +0530

    add route and controller

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv"
-import {connectDB} from "./db"
+import connectDB from "./db/index.js"
+import { app } from "./app.js"
+import e from "express"
 
 dotenv.config({
   path: "./env"
 })
 
 connectDB()
+// .then(() => {
+//   app.listen(process.env.PORT || 8000, () => {
+//       console.log(` Server is running at port : ${process.env.PORT}`);
+//       console.log(` Server is running at port : ${process.env.PORT}`);
+//   })
+// })
+// .catch((err)=>{
+//   console.log('error in connecting mongoDB',err); 
+// })
